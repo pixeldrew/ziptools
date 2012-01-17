043bfb9cab63d0454650779003d06e474f4eef51
Drew Foehn <drew@pixelburn.net>
2012-01-17 15:44:39 -0500

    add jsonp support
    	modified:   app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.enable("jsonp callback");
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
